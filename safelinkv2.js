b6a90d87bc88d5a98128dc1b69e13d09f009f6eb
lihatxxi <faramshop27@gmail.com>
2017-06-30 09:23:00 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -19,12 +19,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
 if(x=="1"){xxx="10-manfaat-puasa-senin-kamis.html"}
 if(x=="2"){xxx="10-cara-mendapatkan-uang-dari-blog-anda.html"}
 if(x=="3"){xxx="7-keutamaan-puasa-daud-yang-luar-biasa.html"}
-if(x=="4"){xxx="cara-meningkatkan-memory-limit-php-di.html"}a_to_ve[i].href="https://lewatlihatxxi.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="cara-meningkatkan-memory-limit-php-di.html"}
+if(x=="5"){xxx="cara-mudah-menerima-pembayaran-dari_15.html"}
+if(x=="6"){xxx="cara-membuat-kotak-banner-iklan-125x125.html"}a_to_ve[i].href="https://lewatlihatxxi.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
